50e8021fd3dd70fcd080756bb85d17b265978b47
Michal Kiral <536474@mail.muni.cz>
2024-03-06 19:01:50 +0100

    Add implementation of repository

---
+++ Backend/Repository/RepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace backend.Repository
+{
+    public abstract class RepositoryBase<T> : Interfaces.IRepositoryBase<T> where T: class 
+    {
+        protected ApplicationDataContext ApplicationDataContext { get; set; }
+
+        protected RepositoryBase(ApplicationDataContext dbContext)
+        {
+            ApplicationDataContext = dbContext;
+        }
+
+        public IQueryable<T> FindAll() => ApplicationDataContext.Set<T>().AsNoTracking();
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression) =>
+            ApplicationDataContext.Set<T>().Where(expression).AsNoTracking();
+        public T Create(T entity) => ApplicationDataContext.Set<T>().Add(entity).Entity;
+        public T Update(T entity) => ApplicationDataContext.Set<T>().Update(entity).Entity;
+        public void Delete(T entity) => ApplicationDataContext.Set<T>().Remove(entity);
+    }
+}
